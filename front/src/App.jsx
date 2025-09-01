99bd75d6bfd6d6560272314db58046bb8ac549a3
furuyamik2 <furuya002.3020junjun@gmail.com>
2025-09-01 22:47:23 +0900

    fix：ファイル名の変更

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Top from './pages/Top';
-import Step3 from './pages/step3';
-import SendMoney from './components/SendMoney';
-
-
-
+import AddressList from './pages/AddressList';
+import SendMoney from './pages/SendMoney';
 
 function App() {
   return (
@@ -13,7 +10,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Top />} />
-          <Route path="/step3" element={<Step3 />} />
+          <Route path="/step3" element={<AddressList />} />
           <Route path="/SendMoney" element={<SendMoney />} />
         </Routes>
       </div>
