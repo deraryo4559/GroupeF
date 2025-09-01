f58e47a0868e9bb2876b71632d5195d9e3c324b3
taichi0901-16 <b.taichi0901@icloud.com>
2025-09-01 16:20:58 +0900

    ルーティングの追加

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-
+import Step3 from './pages/step3';
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/step3" element={<Step3 />} />
         </Routes>
       </div>
     </Router>
